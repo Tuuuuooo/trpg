eec7007b42f1ed78873ff17400de44501453ba36
Tuuuuooo <42348929+Tuuuuooo@users.noreply.github.com>
2021-06-23 23:51:51 +0900

    初回コミット

---
+++ donjon/ItemDrop.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+const jsonObject = JSON.parse(fs.readFileSync('./item.json', 'utf8'));
+
+let Drop = ItemDrop(jsonObject,1)
+
+console.log(Drop)
+
+function ItemDrop(ItemJson, Level) {
+    let totalWeight = 0;
+    let pick = "";
+    let Length = 0;
+
+    //トータルの重みを計算する
+    ItemJson.Item.forEach((obj) => {
+        totalWeight += obj.ratio;
+    });
+
+    //配列の長さを取得する
+    Length = ItemJson.Item.length;
+
+    //抽選する
+    let rnd = Math.floor(Math.random() * totalWeight);
+
+    for (let i = 0; i < Length; i++) {
+            if (rnd < ItemJson.Item[i].ratio) {
+                //出現レベルを確認
+                if(Level === ItemJson.Item[i].level){
+                    // 抽選対象決定
+                    pick = ItemJson.Item[i].name;
+                    break;
+                }else {
+                    continue;
+                }
+            }
+        //次の対象を調べる
+        rnd -= ItemJson.Item[i].ratio;
+    }
+    return pick;
+}
